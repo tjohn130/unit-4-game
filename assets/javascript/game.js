27794892da50028ebd0edbcb17bb99c2115ffa40
Teddy Johnson <tjohn13@email.cpcc.edu>
2018-10-19 17:56:26 -0400

    game fix 1.01

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,26 +8,26 @@ var character = [{name: "J1", posnum: 1, live: true, img: '<img class="player" s
 for(var i=0;i<4;i++){
     $(".gamearea").append(character[i].img);
 }
-var playerclickcheck = true;
+var playerclickcheck = false;
        
 //Start Game
 if(playerclickcheck == false){
     $(".player").on("click", function(){
     $(this).addClass("hero");
     $(this).removeClass("player");
-    $(".battlearea").append($(".player"));
-    $(".player").addClass("enemy");
+    $(".player").addClass("enemy")
+    $(".battlearea").append($(".enemy"));
     $(".player").removeClass("player");
     playerclickcheck = true;
     })
 }
-
-$(".enemy").on("click",function(){
+else{
+    $(".enemy").on("click",function(){
     $(this).addClass("defender");
     $(this).removeClass("enemy")
     $(".defendarea").append($(".defender"));
     })
-
+}
 
 
 
