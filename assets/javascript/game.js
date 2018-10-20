fd60d52d6241f14071dfad41a5f266e84b42f9c8
tjohn130 <tjohn130@email.cpcc.edu>
2018-10-20 14:11:41 -0400

    small fixes to game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,41 @@
 $(document).ready(function(){
 //Character Fighters   
-var character = [{name: "J1", posnum: 1, live: true, img: '<img class="player" src="https://via.placeholder.com/30x30">'},
-    {name:"J2", posnum:2, live:true, img:'<img class="player" src="https://via.placeholder.com/30x30">'},
-    {name:"J2", posnum:3, live:true, img:'<img class="player" src="https://via.placeholder.com/30x30">'},
-    {name:"J2", posnum:4, live:true, img:'<img class="player" src="https://via.placeholder.com/30x30">'}]
+var character = [{name: "J1", hp: 200,ap:15, live: true, img: '<img class="player" value="0" src="https://via.placeholder.com/30x30">'},
+    {name:"J2", hp:10, ap:15, live:true, img:'<img class="player" value="1" src="https://via.placeholder.com/30x30">'},
+    {name:"J3", hp:10, ap:15, live:true, img:'<img class="player" value="2" src="https://via.placeholder.com/30x30">'},
+    {name:"J4", hp:10, ap:15, live:true, img:'<img class="player" value="3" src="https://via.placeholder.com/30x30">'}]
 //Displays Characters
 for(var i=0;i<4;i++){
     $(".gamearea").append(character[i].img);
+    
 }
-var playerclickcheck = true;
+var playerclickcheck = false;
        
 //Start Game
 if(playerclickcheck == false){
-    $(".player").on("click", function(){
-    $(this).addClass("hero");
+    $(".gamearea").on("click", ".player", function(){
     $(this).removeClass("player");
     $(".battlearea").append($(".player"));
-    $(".player").addClass("enemy");
-    $(".player").removeClass("player");
+    $(".player").addClass("enemy").removeClass("player");
+    $(this).addClass("hero");
     playerclickcheck = true;
     })
 }
 
-$(".enemy").on("click",function(){
-    $(this).addClass("defender");
-    $(this).removeClass("enemy")
+$(".battlearea").on("click",".enemy", function(){
+    $(this).removeClass("hero").removeClass("enemy").addClass("defender");
     $(".defendarea").append($(".defender"));
-    })
+    });
+// Attack Button
+$("#attack").on("click",function(){
+    var heroindex = $(".hero").attr("value");
+    var defenderindex = $(".defender").attr("value");
+    heroObj = character[parseInt(heroindex)];
+    defenderObj = character[parseInt(defenderindex)];
 
-
-
-
+    defenderObj.hp = defenderObj.hp - heroObj.ap;
+    console.log("The defender health is "+defenderObj.hp);
     
-    
+})
+
 });
